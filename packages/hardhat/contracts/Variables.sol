1e5c27337a7b61618070650e150c82cb1f777315
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-10-18 15:23:37 +0700

    Add summary of types of Solidity variables

--- packages/hardhat/contracts/Variables.sol
+++ packages/hardhat/contracts/Variables.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+/*
+    There are 3 types of variables in Solidity:
+    
+        - local
+            - declared inside of a function
+            - not stored on the blockchain
+        
+        - state
+            - declared outside of a function
+            - stored on the blockchain
+
+        - global
+            - provides information about the blockchain
+
+*/
+
 contract Variables {
     // State variables are stored on the blockchain.
     string public text = "Hello";

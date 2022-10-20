eb435a1e9d8e27e8242e7e58209ba10361ec11bf
Robert Joseph Wayne <robertjwayne@gmail.com>
2022-10-20 16:32:39 +0700

    Add notes about enums

--- packages/hardhat/contracts/Enum/EnumDeclaration.sol
+++ packages/hardhat/contracts/Enum/EnumDeclaration.sol
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+/*
+    Solidity supports enumerables and they are useful to model choice and keep track of state.
+
+    Enums can be declared outside of a contract.
+*/
+
 enum Status {
     Pending,
     Shipped,
